13c601365fdf0f8673f03b63ecb7e687c0157c2a
James Wendt <jwendt@cs.ucla.edu>
2011-10-19 18:33:48 -0700

    Added new functions to load data, enumerate sensor groupings, compute actual metrics and 'at sensor' metrics, and a preliminary pipeline script that calls these functions

---
+++ GetMetrics.m
@@ -0,0 +1,503 @@
+function [dlds...
+          amp...
+          lateral...
+          heeltoe...
+          dlds_at_sensor...
+          amp_at_sensor...
+          land_at_sensor...
+          step_starts...
+          step_ends...
+          test_dlds...
+          test_amp...
+          test_lateral...
+          test_heeltoe...
+          test_dlds_at_sensor...
+          test_amp_at_sensor...
+          test_land_at_sensor...
+          test_step_starts...
+          test_step_ends] = GetMetrics(input,...
+                                       feet,...
+                                       groupings_list,...
+                                       left_side_indices,...
+                                       right_side_indices,...
+                                       heel_side_indices,...
+                                       toe_side_indices)
+% GetMetrics(FILENAMES, GROUPINGS, LEFT_SIDE_INDICES, RIGHT_SIDE_INDICES, HEEL_SIDE_INDICES, TOE_SIDE_INDICES)
+% 
+%
+%
+%
+%
+%
+%
+%
+%
+%
+%
+
+THRESHOLD_FRACTION = 0.15;
+TESTING_PORTION = 0.2;
+
+% Training Data
+dlds = [];
+amp = [];
+lateral = [];
+heeltoe = [];
+
+dlds_at_sensor = [];
+amp_at_sensor = [];
+land_at_sensor = [];
+
+step_starts = [];
+step_ends = [];
+
+% Testing data
+test_dlds    = [];
+test_amp     = [];
+test_lateral = [];
+test_heeltoe = [];
+
+test_dlds_at_sensor    = [];
+test_amp_at_sensor     = [];
+test_land_at_sensor = [];
+
+test_step_starts = [];
+test_step_ends = [];
+
+% Grab all inputs
+for i = 1:length(input)
+  fprintf('Loading ''%s''\n', input{i});
+  data = load(input{i});
+
+  for f = 1:length(feet)
+    if(strcmp(feet{f},'left'))
+      foot_data = data.left_foot;
+    elseif(strcmp(feet{f},'right'))
+      foot_data = data.right_foot;
+    end
+
+    % Get sample indices of beginning and ending
+    [left_step_starts left_step_ends] = GetSteps(sum(data.left_foot,2), THRESHOLD_FRACTION);
+    [right_step_starts right_step_ends] = GetSteps(sum(data.right_foot,2), THRESHOLD_FRACTION);
+  
+    % Use the indices from GetSensorGroupingsFromPedar to build sensor groupings and
+    % append them to our data matrices. Previously we found left_dlds, left_amp,
+    % etc. using GetMetrics(data.left_foot, ...). Now we append to
+    % data.left_foot the mean values of each sensor grouping.
+    fprintf('Calculating %d data samples for each %d sensor groupings for %s foot\n', size(data.left_foot,1), length(groupings_list), feet{f});
+    [num_samples nada] = size(data.left_foot);
+    left_foot_with_groupings = nan(num_samples, length(groupings_list));
+    for group = 1:length(groupings_list)
+      left_foot_with_groupings(:,group) = mean(data.left_foot(:,groupings_list{group}),2);
+    end
+  
+    % Calculate metrics
+    % TODO: GetMetrics does not know how to compute lateral and heeltoe from
+    %       the sensor groupings defined by GetSensorGroupingsFromPedar (main
+    %       issue is we're calculating lateral using left_side_indices and
+    %       right_side_indices, when really we should only be using the defaults.
+    fprintf('Calculating %s foot metrics\n', feet{f});
+    [left_dlds...
+     left_amp...
+     left_lateral...
+     left_heeltoe...
+     left_dlds_at_sensor...
+     left_amp_at_sensor...
+     left_land_at_sensor] = GetMetricsFromSingleDataSource(left_foot_with_groupings,...
+                                                           left_step_starts,...
+                                                           left_step_ends,...
+                                                           THRESHOLD_FRACTION,...
+                                                           left_side_indices,...
+                                                           right_side_indices,...
+                                                           heel_side_indices,...
+                                                           toe_side_indices);
+    % Plotting functions below are unnecessary
+    %{
+    figure(i); clf; hold on;
+    subplot(2,1,1);
+    plot(sum(data.left_foot,2), '-b');
+    hold on;
+    a = axis; max_amp = a(3); min_amp = a(4);
+    title(sprintf('left S%d', i));
+    for j=1:length(left_step_starts)
+      plot([left_step_starts(j) left_step_starts(j)],[min_amp max_amp],'-g');
+      plot([left_step_ends(j) left_step_ends(j)],[min_amp max_amp],'-k');
+    end
+  
+    subplot(2,1,2);
+    plot(sum(data.right_foot,2), '-r');
+    hold on;
+    a = axis; max_amp = a(3); min_amp = a(4);
+    title(sprintf('right S%d', i));
+    for j=1:length(right_step_starts)
+      plot([right_step_starts(j) right_step_starts(j)],[min_amp max_amp],'-g');
+      plot([right_step_ends(j) right_step_ends(j)],[min_amp max_amp],'-k');
+    end
+    %}
+  
+    % TODO This section is currently a bottleneck. Must speed it up and try to
+    % decrease the memory footprint that it leaves.
+    % Split the data into a training portion and testing portions.
+    fprintf('Splitting data into train and test\n');
+    tmp_dlds = left_dlds;
+    tmp_amp = left_amp;
+    tmp_lateral = left_lateral;
+    tmp_heeltoe = left_heeltoe;
+    tmp_dlds_at_sensor = left_dlds_at_sensor;
+    tmp_amp_at_sensor = left_amp_at_sensor;
+    tmp_land_at_sensor = left_land_at_sensor;
+  
+    testing_length = ceil(TESTING_PORTION*length(tmp_dlds));
+  
+    test_dlds = [test_dlds; tmp_dlds(1:testing_length)];
+    test_amp = [test_amp; tmp_amp(1:testing_length)];
+    test_lateral = [test_lateral; tmp_lateral(1:testing_length)];
+    test_heeltoe = [test_heeltoe; tmp_heeltoe(1:testing_length)];
+  
+    test_dlds_at_sensor = [test_dlds_at_sensor; tmp_dlds_at_sensor(1:testing_length,:)];
+    test_amp_at_sensor = [test_amp_at_sensor; tmp_amp_at_sensor(1:testing_length,:)];
+    test_land_at_sensor = [test_land_at_sensor; tmp_land_at_sensor(1:testing_length,:,:)];
+  
+    test_step_starts = [test_step_starts left_step_starts(1:testing_length)];
+    test_step_ends = [test_step_ends left_step_ends(1:testing_length)];
+  
+    dlds = [dlds; tmp_dlds(testing_length+1:length(tmp_dlds))];
+    amp = [amp; tmp_amp(testing_length+1:length(tmp_amp))];
+    lateral = [lateral; tmp_lateral(testing_length+1:length(tmp_lateral))];
+    heeltoe = [heeltoe; tmp_heeltoe(testing_length+1:length(tmp_heeltoe))];
+  
+    dlds_at_sensor = [dlds_at_sensor; tmp_dlds_at_sensor(testing_length+1:length(tmp_dlds),:)];
+    amp_at_sensor = [amp_at_sensor; tmp_amp_at_sensor(testing_length+1:length(tmp_amp),:)];
+    land_at_sensor = [land_at_sensor; tmp_land_at_sensor(testing_length+1:length(tmp_lateral),:,:)];
+    
+    step_starts = [step_starts left_step_starts(testing_length+1:length(left_step_starts))];
+    step_ends = [step_ends left_step_ends(testing_length+1:length(left_step_ends))];
+  
+    % TODO implement testing_portion and training_portion over the five sets of
+    % possible testing and training portions (five because we do 80/20 training
+    % to testing ratio)
+    %testing_length = ceil(TESTING_PORTION*length(tmp_dlds));
+    %count = 0;
+    %for start = 0:testing_length:length(tmp_dlds)
+    %  count = count + 1;
+    %  theend = start+testing_length;
+    %  if start+testing_length > length(tmp_dlds)
+    %    theend = length(tmp_dlds);
+    %  end
+    %  testing_indices = start+1:theend;
+    %  test_portion_dlds{count} = [test_portion_dlds{count}; tmp_dlds(testing_indices)];
+    %  test_portion_amp{count} = [test_portion_amp{count}; tmp_amp(testing_indices)];
+    %  test_portion_lateral{count} = [test_portion_lateral{count}; tmp_lateral(testing_indices)];
+    %
+    %  training_indices_A = 1:start;
+    %  training_indices_B = theend+1:length(tmp_dlds);
+    %  if training_indices_A
+    %    train_portion_dlds{count} = [train_portion_dlds{count}; tmp_dlds(training_indices_A)];
+    %    train_portion_amp{count} = [train_portion_amp{count}; tmp_amp(training_indices_A)];
+    %    train_portion_lateral{count} = [train_portion_lateral{count}; tmp_lateral(training_indices_A)];
+    %  end
+    %  if training_indices_B
+    %    train_portion_dlds{count} = [train_portion_dlds{count}; tmp_dlds(training_indices_B)];
+    %    train_portion_amp{count} = [train_portion_amp{count}; tmp_amp(training_indices_B)];
+    %    train_portion_lateral{count} = [train_portion_lateral{count}; tmp_lateral(training_indices_B)];
+    %  end
+    %end
+
+    clear -regexp 'tmp*'
+    clear left_foot_with_groupings
+  end
+  clear data
+end
+
+clear -regexp 'left_'
+
+
+% JAMES HACK to elimate crappy testing data
+% 1. load Saro's output.mat or good_results.mat
+% 2. run prerun.m
+% 3. run print_results.m
+%{
+start = 1;
+%finish = 50;
+finish = length(test_dlds);
+test_dlds = test_dlds(start:finish);
+test_amp = test_amp(start:finish);
+test_lateral = test_lateral(start:finish);
+test_heeltoe = test_heeltoe(start:finish);
+
+test_dlds_at_sensor = test_dlds_at_sensor(start:finish,:);
+test_amp_at_sensor = test_amp_at_sensor(start:finish,:);
+test_land_at_sensor = test_land_at_sensor(start:finish,:,:);
+
+test_step_starts = test_step_starts(start:finish);
+test_step_ends = test_step_ends(start:finish);
+%}
+% SEMAJ
+
+end
+
+
+function [dlds...
+          amp...
+          lateral...
+          heeltoe...
+          dlds_at_sensor...
+          amp_at_sensor...
+          land_at_sensor] = GetMetricsFromSingleDataSource(data,...
+                                                           step_starts,...
+                                                           step_ends,...
+                                                           threshold_percent,...
+                                                           varargin)
+% GetMetricsFromSingleDataSource(DATA, STEP_STARTS, STEP_ENDS, THREHOLD)
+%
+% GetMetricsFromSingleDataSource(DATA, STEP_STARTS, STEP_ENDS, THREHOLD, LEFT_SIDE_INDICES, RIGHT_SIDE_INDICES, HEEL_SIDE_INDICES, TOE_SIDE_INDICS)
+%
+%    GetMetricsFromSingleDataSource returns measurements for change in step stride (dlds),
+%    maximum amplitude (amp), lateral difference between left and right
+%    sides of the foot, and time difference between heel and toe landing (not
+%    yet implemented). It also returns those values measured at each sensor.
+%    length(STEP_STARTS) and length(STEP_ENDS) must be the same.
+LoadConstants;
+if (length(varargin) == 4)
+  left_side_indices = varargin{1};
+  right_side_indices = varargin{2};
+  heel_side_indices = varargin{3};
+  toe_side_indices = varargin{4};
+elseif (length(varargin) == 0)
+  left_side_indices = config.left_side_indices;
+  right_side_indices = config.right_side_indices;
+  heel_side_indices = config.heel_side_indices;
+  toe_side_indices = config.toe_side_indices;
+else
+  error('Must include left side, right side, heel side and toe side incides, or none at all');
+end
+
+EXTRA = 20;
+
+num_steps = length(step_ends);
+[num_samples num_sensors] = size(data);
+
+dlds_at_sensor = zeros(num_steps, num_sensors);
+amp_at_sensor = zeros(num_steps, num_sensors);
+lateral_at_sensor = nan(num_steps, num_sensors, num_sensors);
+heeltoe_at_sensor = nan(num_steps, num_sensors, num_sensors);
+
+amp = zeros(num_steps,1);
+dlds = zeros(num_steps,1);
+lateral = nan(num_steps,1);
+heeltoe = nan(num_steps,1);
+
+%step
+fprintf('  amp & dlds\n');
+for mi=1:num_steps
+  running_amp_sum = 0;
+
+  count = 0;
+  %sensor
+  for si=1:num_sensors
+    % dlds_at_sensor
+    if(mi+1 < num_steps)
+      lower = step_starts(mi) - EXTRA;
+      upper = step_ends(mi+2) + EXTRA;
+      if( upper > num_samples )
+        upper = num_samples;
+      end
+      if( lower < 1 )
+        lower = 1;
+      end
+      if( lower < upper )
+        dlds_at_sensor(mi, si) = ChangeInLength(data(lower:upper,si), threshold_percent);
+      end
+    end
+
+    % amp & amp_at_sensor
+    lower = step_starts(mi);
+    upper = step_ends(mi);
+    if(upper > num_samples)
+      upper = num_samples;
+      fprintf('upper > num_samples!\n');
+    end
+    if(lower < upper)
+      amp_at_sensor(mi, si) = max(data(lower:upper, si));
+      running_amp_sum = running_amp_sum + max(data(lower:upper, si));
+    end
+  end
+
+  amp(mi) = running_amp_sum/num_sensors;
+
+  if(mi+1 < num_steps)
+    dlds(mi) = (step_starts(mi+2) - step_starts(mi+1)) - (step_starts(mi+1) - step_starts(mi));
+  end
+end
+
+% now that we've calculated all of the amp_at_sensor values, we use them
+% to calculate the lateral_at_sensor and lateral values
+fprintf('  lateral\n');
+for mi=1:num_steps
+  % Nifty little bit of code here essentially does a cross product of the
+  % left_sensor_amp and right_sensor_amp vectors using the division function
+  % between them (L ./ R)
+  left_sensor_amp = amp_at_sensor(mi, left_side_indices)';
+  right_sensor_amp = amp_at_sensor(mi, right_side_indices);
+  L = repmat(left_sensor_amp, 1, length(right_sensor_amp));
+  R = repmat(right_sensor_amp, length(left_sensor_amp), 1);
+  lateral_at_sensor(mi, left_side_indices, right_side_indices) = L - R;
+
+  %lateral(mi) = sum(amp_at_sensor(mi,left_side_indices)) / sum(amp_at_sensor(mi, right_side_indices));
+  % We only use config.left_side_indices and config.right_side_indices to
+  % accurately describe the lateral difference
+  lateral(mi) = mean(amp_at_sensor(mi,config.left_side_indices)) - mean(amp_at_sensor(mi, config.right_side_indices));
+end
+
+fprintf('  heeltoe\n');
+% Compute the land_at_sensor values by inspecting the step 
+land_at_sensor = nan(num_steps, num_sensors);
+for mi=1:num_steps
+  for si=1:num_sensors
+    lower = step_starts(mi) - 1;
+    upper = step_ends(mi) + 1;
+    if(upper > num_samples)
+      upper = num_samples;
+      fprintf('upper > num_samples!\n');
+    end
+    if(lower < upper)
+      threshold = threshold_percent * max(data(lower:upper, si));
+      for di=lower:upper
+        if(data(di, si) > threshold)
+          land_at_sensor(mi, si) = di;
+          break;
+        end
+      end
+    end
+  end
+end
+
+for mi=1:num_steps
+  % Nifty little bit of code here essentially does a cross product of the
+  % heel_sensor_land and toe_sensor_land vectors for a given step using the
+  % subtraction function between them (H - T)
+  heel_sensor_land = land_at_sensor(mi, heel_side_indices)';
+  toe_sensor_land = land_at_sensor(mi, toe_side_indices);
+  H = repmat(heel_sensor_land, 1, length(toe_sensor_land));
+  T = repmat(toe_sensor_land, length(heel_sensor_land), 1);
+  heeltoe_at_sensor(mi, heel_side_indices, toe_side_indices) = H - T;
+
+  % Use only config.heel_side_indices and config.toe_sideindices to accurately
+  % define the heeltoe metric
+  heel_sensor_land = land_at_sensor(mi, config.heel_side_indices);
+  toe_sensor_land = land_at_sensor(mi, config.toe_side_indices);
+
+  % Use mean instead of min because min was much too noisy
+  heeltoe(mi) = mean(heel_sensor_land(~isnan(heel_sensor_land))) - mean(toe_sensor_land(~isnan(toe_sensor_land)));
+end
+
+end
+
+
+function dlds = ChangeInLength(data, threshold_percent);
+% ChangeInLength
+%    ChangeInLength(D, T) finds the change in length of steps between each
+%    step in the raw data, D. The start of a step occurs when the signal
+%    in D rises above T*max(D). The step ends when the signal in D falls
+%    below T*max(D).
+
+threshold = threshold_percent*max(data);
+
+% set stepping flags
+marked_in_air = (data(1) <= threshold);
+in_air = (data(1) <= threshold);
+%in_air = false;
+%marked_in_air = false;
+
+num_samples = length(data);
+
+number_of_steps = 0;
+for i=1:num_samples
+  % data was in the air, now the pressure is large (step beginning)
+  %if(in_air(sensor_index) && data(i,sensor_index) > threshold_step)
+  if(in_air && data(i) > threshold)
+    in_air = false;
+
+    if(marked_in_air)
+      marked_in_air = false;
+
+      % update so we know where the step start was
+      number_of_steps = number_of_steps + 1;
+      step_start_sum(number_of_steps) = i;
+      %plot([i i],[min_amplitude max_amplitude],'-g');
+    end
+
+  % data is on the ground, pressure is now relieving (step ending)
+  %elseif(~in_air(sensor_index) && data(i,sensor_index) <= threshold_step)
+  elseif(~in_air && data(i) <= threshold)
+    in_air = true;
+    if(~marked_in_air)
+      marked_in_air = true;
+
+      % update so we know where the step ends
+      if(number_of_steps ~= 0)
+        step_end_sum(number_of_steps) = i;
+        %plot([i i],[min_amplitude max_amplitude],'-r');
+        %fprintf('sensor : step @ %d, time %f\n', i, time(i));
+      end
+    end
+  end
+end
+
+%fprintf('number_of_steps = %d, \n', number_of_steps);
+if(number_of_steps < 3)
+  dlds = -999999; %nan; %-9999
+else
+  dlds = (step_start_sum(3) - step_start_sum(2)) - (step_start_sum(2) - step_start_sum(1));
+end
+
+end
+
+
+function [step_starts step_ends] = GetSteps(data, threshold_fraction)
+% GetSteps(DATA, THRESHOLD_FRACTION)
+%   Returns the step start indices and step end indices given a vector pressure
+%   data points. When the data passes the THRESHOLD_FRACTION of it's max data
+%   point, a step beings. When the data cross below the THRESHOLD_FRACTION, the
+%   step ends.
+threshold = threshold_fraction*max(data);
+
+num_samples = length(data);
+
+in_air = (data(1) <= threshold);
+marked_in_air = (data(1) <= threshold);
+
+number_of_steps = 0;
+for i=1:num_samples
+  % data was in the air, now the pressure is large (step beginning)
+  %if(in_air(sensor_index) && data(i,sensor_index) > threshold_step)
+  if(in_air && data(i) > threshold)
+    in_air = false;
+
+    if(marked_in_air)
+      marked_in_air = false;
+
+      % update so we know where the step start was
+      number_of_steps = number_of_steps + 1;
+      step_starts(number_of_steps) = i;
+      %plot([i i],[min_amplitude max_amplitude],'-g');
+    end
+
+  % data is on the ground, pressure is now relieving (step ending)
+  %elseif(~in_air(sensor_index) && data(i,sensor_index) <= threshold_step)
+  elseif(~in_air && data(i) <= threshold)
+    in_air = true;
+    if(~marked_in_air)
+      marked_in_air = true;
+
+      % update so we know where the step ends
+      if(number_of_steps > 0) % ensures that we always begin with a starting step, not an ending one
+        step_ends(number_of_steps) = i;
+      end
+      %plot([i i],[min_amplitude max_amplitude],'-r');
+      %fprintf('sensor %d: step @ %d, time %f\n', sensor_index, i, time(i));
+    end
+  end
+end
+
+end
---
+++ GetSensorGroupingsFromPedar.m
@@ -0,0 +1,163 @@
+function [groupings_list...
+          groupings_matrix...
+          left_side_indices...
+          right_side_indices...
+          heel_side_indices...
+          toe_side_indices] = GetSensorGroupingsFromPedar(varargin)
+% GetSensorGroupingsFromPedar   Returns a cell array of N sensor groupings (vectors) as
+%                               well as a Nx99 matrix of logicals in which the (i,j)
+%                               element is 1 if group i contains sensor j and 0
+%                               otherwise. The sensor groupings are created by applying
+%                               the variable number of variable sized matrices
+%                               exhaustively across the Pedar shoe sensor mapping
+%                               matrix.
+%
+%                               Calling GetSensorGroupingsFromPedar with no
+%                               masks passed in will automatically return shoe
+%                               mappings for the single sensor. Therefore, it
+%                               is not necessary to pass the single sensor in
+%                               as a parameter.
+%
+%    GetSensorGroupingsFromPedar(VARARGIN)
+%
+%    VARARGIN is any number of arbitrarily sized matrices of logicals (1s and
+%             0s). These masks are applied on the equivalently sized
+%             sub-matrices of the Pedar shoe sensor mapping matrix.
+%
+%    Example masks include the following:
+%
+%      masks{1} = [1 1];
+%
+%      masks{2} = [1;
+%                  1];
+%
+%      masks{3} = [1 1;
+%                  1 1];
+%
+%      masks{4} = [1 1 1 1;
+%                  1 1 1 1];
+%
+%      masks{5} = [1 1;
+%                  1 1;
+%                  1 1;
+%                  1 1];
+%
+%      masks{6} = [1 1 0 0;
+%                  1 1 0 0;
+%                  1 1 1 1;
+%                  1 1 1 1];
+%
+%      masks{7} = rot90(masks{6});
+%
+%      masks{8} = rot90(masks{7});
+%
+%      masks{9} = rot90(masks{8});
+%
+%
+%    Example calling the function:
+%
+%      [groupings_list groupings_matrix] = GetSensorGroupingsFromPedar(masks{1}, masks{2});
+%
+%      [groupings_list groupings_matrix] = GetSensorGroupingsFromPedar(masks{:});
+
+% shoe_image represents the sensors on the Hermes shoe platform for the right
+% foot. The left foot is simply the reflection of this matrix about the y axis,
+% this means no transformation from left to right is necessary. E.g. sensor
+% 1 corresponds to the inner heel on both the left and right foot.
+shoe_image = [ NaN 96 97 98 99  NaN  NaN;
+              90 91 92 93 94 95  NaN;
+              83:89;
+              76:82;
+              69:75;
+              62:68;
+              55:61;
+              48:54;
+              41:47;
+              34:40;
+              27:33;
+              20:26;
+              13:19;
+              6:12;
+              NaN 1:5 NaN];
+
+% Get left, right, heel, and toe indices, from here, we work
+LoadConstants;
+shoe_right = config.right_side_indices;
+shoe_left = config.left_side_indices;
+shoe_heel = config.heel_side_indices;
+shoe_toe = config.toe_side_indices;
+
+% Flip shoe upside down just so that we always start our groupings enumerations
+% with sensor 1. This step is not necessary, but helps a little with radability
+% if/when debugging the sensor groupings.
+shoe_image = flipud(shoe_image);
+[shoe_length shoe_width] = size(shoe_image);
+
+num_groupings = 0;
+groupings_list = {};
+groupings_matrix = [];
+left_side_indices = [];
+right_side_indices = [];
+heel_side_indices = [];
+toe_side_indices = [];
+
+masks = {[1] varargin{:}};
+
+% Apply each mask to each same sized sub-matrix in the Pedar shoe mapping
+for grouping_index = 1:size(masks,2)
+  mask = logical(masks{grouping_index});
+  [mask_length mask_width] = size(mask);
+
+  % Check that the mask dimensions fall within the shoe matrix
+  if(mask_length > shoe_length || mask_width > shoe_width)
+    fprintf('Error: sensor mask dimensions must be less than or equal to the Pedar shoe dimensions (about 15x7 to an approximation)\n');
+    clear groupings_list, groupings_matrix;
+    return
+  end
+
+  % Loop through entire shoe (but only so far as the mask dimensions allow)
+  for i=1:shoe_length-mask_length+1
+    for j=1:shoe_width-mask_width+1
+      % Grab a temporary portion of the shoe that has the same dimension as the
+      % mask
+      shoe_portion = shoe_image(i:i+mask_length-1, j:j+mask_width-1);
+
+      % Grab the sensors in this shoe_portion corresponding to the mask and
+      % convert that matrix to a vector.
+      sensors = shoe_portion(mask);
+      sensors = reshape(sensors,1,numel(sensors));
+      sensors = sort(sensors);
+
+      % Only add sensor groupings to the list/matrix if the mask successfully
+      % overlays only sensors
+      if(sum(isnan(sensors)) == 0)
+        num_groupings = num_groupings + 1;
+
+        % Append sensor list to groupings list
+        groupings_list{num_groupings} = sensors;
+
+        % Append row to groupings matrix where columns corresponding to sensor
+        % list are set to 1
+        groupings_matrix = [groupings_matrix; zeros(1,99)];
+        groupings_matrix(num_groupings, sensors) = 1;
+
+        % If more of the groupings are in the shoe_left than in shoe_right then
+        % we deem this sensor grouping to be on the left side, vice versa for
+        % the right side, and likewise for the heel and toe side
+        if(sum(ismember(sensors, shoe_left)) > sum(ismember(sensors, shoe_right)))
+          left_side_indices = [left_side_indices num_groupings];
+        elseif(sum(ismember(sensors, shoe_right)) > sum(ismember(sensors, shoe_left)))
+          right_side_indices = [right_side_indices num_groupings];
+        end
+
+        if(sum(ismember(sensors, shoe_heel)) > sum(ismember(sensors, shoe_toe)))
+          heel_side_indices = [heel_side_indices num_groupings];
+        elseif(sum(ismember(sensors, shoe_toe)) > sum(ismember(sensors, shoe_heel)))
+          toe_side_indices = [toe_side_indices num_groupings];
+        end
+      end
+    end
+  end
+end
+
+groupings_matrix = logical(groupings_matrix);
