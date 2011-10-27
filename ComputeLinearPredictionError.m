d7ceefb44ffae8c2ea8e495da875ea7b9cdcae23
James Wendt <jwendt@cs.ucla.edu>
2011-10-26 18:17:45 -0700

    Fixed up comments in ComputeLinearPredictionError

--- ComputeLinearPredictionError.m
+++ ComputeLinearPredictionError.m
@@ -1,16 +1,19 @@
 function err = ComputeLinearPredictionError(train, train_predictors, test, test_predictors)
 % ComputeLinearPreditionError
-%    Finds the linear equation to apply 
-%                             and the train_predictors' measurements. The error is
-%                             computed by measuring the differences between the
-%                             train values and the linear model trained from
-%                             the train_predictors and the train values.
+%     Takes in a training dataset along with the values of the training
+%     predictors, computes the linear equation to apply to a row vector of such
+%     training predictors, then applys this equation to the testing predictors
+%     and returns the error of these predictions on the testing vector.
 %
-%    ComputeLinearPredictionError(TRAIN, TRAIN_PREDICTORS)
+%    ComputeLinearPredictionError(TRAIN, TRAIN_PREDICTORS, TEST, TEST_PREDICTORS)
 %
-%      TRAIN is a vector of size N of train values
+%      TRAIN is a vector of size N of training values
 %
 %      TRAIN_PREDICTORS is a matrix of size NxM with M predictor measurements
+%
+%      TEST is a vector of size P of training values
+%
+%      TEST_PREDICTORS is a matrix of size PxM with M predictor measurements
 [num_observations num_train_predictors] = size(train_predictors);
 Xtrain = [ones(size(train)) train_predictors];
 b = regress(train, Xtrain);
